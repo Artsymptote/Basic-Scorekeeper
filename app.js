9a765547091bb96f84448862112d18611494da37
Artsymptote <Artsymptote@users.noreply.github.com>
2022-12-11 00:37:15 -0800

    Fully functioning now.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const score2 = document.querySelector(".score2");
 
 //Drop-down & selection
 const rounds = document.querySelector("#rounds");
-const matchPtDeterminer = Number(rounds.value);
 
 //Buttons
 const btn1 = document.querySelector(".btn1");
@@ -13,8 +12,7 @@ const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
 
 //PLANS
-//TODO 1. Connect the match-count dropdown with the display numbers.I want the drop down to determine which number goes red when.
-//? can we listen for the number that is selected rather than the selection iself? FOR EXAMPLE can we use event bubbling to listen for which score digit changes-- listen to the <text> element for changes in the <span> elements?
+//*DONE! Connect the match-count dropdown with the display numbers.I want the drop down to determine which number goes red when.
 //TODO 2. I want the counting to stop working when one of the numbers reaches the match-end.
 //?when btn1 OR btn2 .innerText reaches dropdown # innerText the buttons no longer connect to the numbers?
 //* DONE! I need a counter that will keep the score.
@@ -25,9 +23,12 @@ btn1.addEventListener("click", function () {
 	let scoreNum1 = Number(score1.innerText); //counter for the number
 	scoreNum1 += 1;
 	score1.innerText = scoreNum1;
-	if (matchPtDeterminer != 0 && scoreNum1 == matchPtDeterminer) {
+	if (Number(rounds.value) !== 0 && scoreNum1 === Number(rounds.value)) {
 		score1.style.color = "green";
 		score2.style.color = "red";
+		//Disabling button
+		btn1.disabled = true;
+		btn2.disabled = true;
 	}
 });
 
@@ -38,9 +39,12 @@ btn2.addEventListener("click", function () {
 	let scoreNum2 = Number(score2.innerText); //counter for the number
 	scoreNum2 += 1;
 	score2.innerText = scoreNum2;
-	if (matchPtDeterminer != 0 && scoreNum2 == matchPtDeterminer) {
+	if (Number(rounds.value) !== 0 && scoreNum2 === Number(rounds.value)) {
 		score1.style.color = "red";
 		score2.style.color = "green";
+		//Disabling button
+		btn1.disabled = true;
+		btn2.disabled = true;
 	}
 });
 
@@ -51,6 +55,8 @@ btn3.addEventListener("click", function () {
 	score1.style.color = "black";
 	score2.style.color = "black";
 	rounds.selectedIndex = 0;
+	btn1.disabled = false;
+	btn2.disabled = false;
 });
 
 //Rounds
@@ -61,11 +67,3 @@ scoresBox.addEventListener("change", function (e) {
 		e.target.style.color = "green;";
 	}
 });
-
-// rounds.addEventListener("change", function () {
-// 	console.log(`Playing to ${matchPtDeterminer}`);
-// 	console.log(`Player 1: ${score1.innerText}, Player 2: ${score2.innerText}`);
-// 	let playerOne = score1.innerText;
-// 	let playerTwo = Number(score2.innerText);
-// 	console.log(`${playerOne}${playerTwo}`);
-// });
