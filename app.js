2046805fbef20624abd3de75e7290ce8cb1f5d15
Artsymptote <Artsymptote@users.noreply.github.com>
2022-12-12 13:12:21 -0800

    adding changes to reflect Colt's methodology. Paused to check main brnch

--- app.js
+++ app.js
@@ -11,43 +11,40 @@ const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
 
-//PLANS
-//*DONE! Connect the match-count dropdown with the display numbers.I want the drop down to determine which number goes red when.
-//TODO 2. I want the counting to stop working when one of the numbers reaches the match-end.
-//?when btn1 OR btn2 .innerText reaches dropdown # innerText the buttons no longer connect to the numbers?
-//* DONE! I need a counter that will keep the score.
-//* DONE! I need a reset button that resets the numbers, colors, and match-point.
-
 //Button-to-number
+
+let scoreNum1 = 0; //counter for the number
+let scoreNum2 = 0;
+let matchPtDeterminer = Number(rounds.value);
+
 btn1.addEventListener("click", function () {
-	let scoreNum1 = Number(score1.innerText); //counter for the number
-	scoreNum1 += 1;
-	score1.innerText = scoreNum1;
-	let matchPtDeterminer = Number(rounds.value);
-	if (matchPtDeterminer !== 0 && scoreNum1 === matchPtDeterminer) {
-		score1.style.color = "green";
-		score2.style.color = "red";
-		//Disabling button
-		btn1.disabled = true;
-		btn2.disabled = true;
+	if (scoreNum1 !== matchPtDeterminer) {
+		scoreNum1 += 1;
+		score1.innerText = scoreNum1;
 	}
+
+	// if (matchPtDeterminer !== 0 && scoreNum1 === matchPtDeterminer) {
+	// 	score1.style.color = "green";
+	// 	score2.style.color = "red";
+	// 	//Disabling button
+	// 	btn1.disabled = true;
+	// 	btn2.disabled = true;
+	// }
 });
 
 btn2.addEventListener("click", function () {
-	console.dir(rounds);
-	console.dir(score2);
-
-	let scoreNum2 = Number(score2.innerText); //counter for the number
-	scoreNum2 += 1;
-	score2.innerText = scoreNum2;
-	let matchPtDeterminer = Number(rounds.value);
-	if (matchPtDeterminer !== 0 && scoreNum2 === matchPtDeterminer) {
-		score1.style.color = "red";
-		score2.style.color = "green";
-		//Disabling button
-		btn1.disabled = true;
-		btn2.disabled = true;
+	if (scoreNum2 !== matchPtDeterminer) {
+		scoreNum2 += 1;
+		score2.innerText = scoreNum2;
 	}
+
+	// if (matchPtDeterminer !== 0 && scoreNum2 === matchPtDeterminer) {
+	// 	score1.style.color = "red";
+	// 	score2.style.color = "green";
+	// 	//Disabling button
+	// 	btn1.disabled = true;
+	// 	btn2.disabled = true;
+	// }
 });
 
 //Reset
@@ -64,8 +61,8 @@ btn3.addEventListener("click", function () {
 //Rounds
 //TODO 1. I want the drop down to determine which number goes red,when.
 
-scoresBox.addEventListener("change", function (e) {
-	if (Number(e.target.innerText) === Number(rounds.value)) {
-		e.target.style.color = "green;";
-	}
-});
+// scoresBox.addEventListener("change", function (e) {
+// 	if (Number(e.target.innerText) === Number(rounds.value)) {
+// 		e.target.style.color = "green;";
+// 	}
+// });
