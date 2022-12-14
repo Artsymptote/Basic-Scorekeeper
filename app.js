e7fdf069dc5c4bed50f6138ff45e9d62566446b0
Artsymptote <Artsymptote@users.noreply.github.com>
2022-12-13 18:46:39 -0800

    Refactoring = now has a cascade of functions. Unsure if better or worse.

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 const score1 = document.querySelector(".score1");
 const score2 = document.querySelector(".score2");
 
+//Score counters
+let scoreNum1 = 0; //! not const!
+let scoreNum2 = 0;
+
 //Drop-down & selection
 const rounds = document.querySelector("#rounds");
-let matchPtDeterminer = Number(rounds.value);
+let matchPtDeterminer = Number(rounds.value); //! not const!
 
 //Buttons
 const btn1 = document.querySelector(".btn1");
@@ -13,17 +17,15 @@ const btn3 = document.querySelector(".btn3");
 
 //Button-to-number
 btn1.addEventListener("click", function () {
-	let scoreNum1 = Number(score1.innerText); //counter for the number
 	scoreNum1 += 1;
 	score1.innerText = scoreNum1;
-	gamePoint(scoreNum1);
+	gamePoint();
 });
 
 btn2.addEventListener("click", function () {
-	let scoreNum2 = Number(score2.innerText); //counter for the number
 	scoreNum2 += 1;
 	score2.innerText = scoreNum2;
-	gamePoint(scoreNum2);
+	gamePoint();
 });
 
 //Reset
@@ -38,12 +40,24 @@ btn3.addEventListener("click", function () {
 });
 
 //External Functions
-function gamePoint(theScore) {
-	if (matchPtDeterminer !== 0 && theScore === matchPtDeterminer) {
+function gamePoint() {
+	if (
+		(matchPtDeterminer && scoreNum1 === matchPtDeterminer) ||
+		scoreNum2 === matchPtDeterminer
+	) {
+		showWinner();
+	}
+}
+
+function showWinner() {
+	if (scoreNum1 > scoreNum2) {
+		//Color-changes
+		score1.style.color = "green";
+		score2.style.color = "red";
+	} else {
 		score1.style.color = "red";
 		score2.style.color = "green";
-		//Disabling button
-		btn1.disabled = true;
-		btn2.disabled = true;
 	}
+	btn1.disabled = true; //Disabling buttons
+	btn2.disabled = true;
 }
